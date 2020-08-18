d9e5bbc6667eac32bb099074fa27722f63202a55
Christabel Escarez <christabel.escarez@gmail.com>
2020-08-18 03:09:34 -0700

    program handles ties, appropriate pluralization of votes in final tally, handles more than three candidates; write-in votes, > 10 votes, DRY to be implemented

--- election_time.rb
+++ election_time.rb
@@ -6,18 +6,12 @@
 
 #MAIN
 puts "Please ten voters and vote for the top cereal."
-nominees = [["Special K", 0], ["Lucky Charms", 0], ["Cinnamon Toast Crunch", 0], ["Bacon", 0], ["Write-in Vote", 0]]
+nominees = [["Special K", 0, 'A'], ["Lucky Charms", 0, 'B'], ["Cinnamon Toast Crunch", 0, 'C'], ["Bacon", 0, 'D'], ["Write-in Vote", 0, 'E']]
 
-letter = 'A'
-nominees.each do |cereal_index|
-  cereal_index.each do |cereal_array|
-    cereal_array.each do |cereal_detail|
-      puts "#{letter}: #{cereal_detail[0]}"
-      cereal_detail << letter
-      puts cereal_detail[0] + cereal_detail[1] + cereal_detail[2] #################
-    end
-  end
-  letter.next!
+last_letter = 'A'
+nominees.each do |cereal_array|
+  puts "#{cereal_array[2]}: #{cereal_array[0]}"
+  last_letter = cereal_array[2]
 end
 
 voter_num = 0
@@ -25,38 +19,42 @@ while voter_num < 10
   voter_num += 1
   print "Selection for Voter ##{voter_num}: "
   input_vote = gets.chomp.upcase
-  until (('A'..letter).include?(input_vote))  
+  until (('A'..last_letter).include?(input_vote))  
     puts "Invalid input. Please enter a valid selection."
     input_vote = gets.chomp.upcase
   end
-
-  nominees.each do |cereal_index|
-    cereal_index.each do |cereal_name|
-      if cereal_name[2] == input_vote
-        cereal_name[1].to_i += 1
-      end
+  nominees.each do |cereal_array|
+    if cereal_array[2] == input_vote
+      cereal_array[1] += 1
     end
   end
-end
 
+end
 
 winning_cereal = ""
-highest_vote_count = 0
-nominees.each do |cereal_index|
-  cereal_index.each do |cereal_array|
-    cereal_array.each do |cereal_detail|
-      puts "#{cereal_detail[0]}: #{cereal_detail[1]}"
-      if cereal_detail[1].to_i >= highest_vote_count
-        highest_vote_count = cereal_detail[1].to_i
-        winning_cereal = cereal_detail[0] 
-      elsif cereal_detail[1].to_i == highest_vote_count
-        winning_cereal << cereal_detail[0] 
-      end
-    end
+highest_vote_count = multiple_winners = 0
+puts "The final tally:"
+nominees.each do |cereal_array|
+  if cereal_array[1] == 1
+    puts "#{cereal_array[0]} - #{cereal_array[1]} vote"
+  else
+    puts "#{cereal_array[0]} - #{cereal_array[1]} votes"
+  end
+  if cereal_array[1] > highest_vote_count
+    highest_vote_count = cereal_array[1]
+    winning_cereal = cereal_array[0] 
+  elsif cereal_array[1].to_i == highest_vote_count
+    winning_cereal += " and " + cereal_array[0] 
+    multiple_winners = true
   end
 end
+
+if multiple_winners
+  puts "The winning cereals are #{winning_cereal}!"
+else
+  puts "The winning cereal is #{winning_cereal}!"
+end
   
-puts "The winning cereal is #{winning_cereal}"
   
 
 # nominees = [{selection_id => 'A', "Special K" => [0, 'A']}, {selection_id => 'B', "Lucky Charms" => 0}, {selection_ID => 'C', "Cinnamon Toast Crunch" => 0}, {selection_id => 'D', "Bacon" => 0}, {selection_id => 'E', "Write-in Vote" => 0}]
@@ -71,4 +69,15 @@ puts "The winning cereal is #{winning_cereal}"
   #   i++
   # end
   # nominees[selection_index] += 1
-  
+# letter = 'A'
+# nominees.each do |cereal_array|
+#   # puts cereal_array[0] + cereal_array[1] + cereal_array[2] #################
+#   puts cereal_array[0]
+#   cereal_array.each do |cereal_detail|
+#     puts "#{letter}: #{cereal_detail}"
+#     cereal_detail << letter
+#     puts cereal_detail[0] + cereal_detail[1] + cereal_detail[2] #################
+#     letter.next!
+#   end
+# end
+
