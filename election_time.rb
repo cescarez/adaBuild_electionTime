7790ccf3c648a1349f06e1a38297909baecf165b
Christabel Escarez <christabel.escarez@gmail.com>
2020-08-17 22:49:34 -0700

    started implementing arrays + iteration

--- election_time.rb
+++ election_time.rb
@@ -5,34 +5,38 @@
 
 #MAIN
 puts "Please ten voters and vote for the top cereal."
-nominees = ["Special K", "Lucky Charms", "Cinnamon Toast Crunch", "Bacon", "Write-in Vote"]
-letter = '@'
+nominees = [{"Special K" => 0}, {"Lucky Charms" => 0}, {"Cinnamon Toast Crunch" => 0}, {"Bacon" => 0}, {"Write-in Vote" => 0}]
+letter = 'A'
+# letter_array = Array.new
 nominees.each do |cereal|
-  puts "#{letter.next}: #{cereal}"
+  cereal.each_key do |cereal_name|
+    puts "#{letter}: #{cereal_name}"
+  end
+  letter.next
+  puts letter
 end
 
 voter_num = 0
-A_count = B_count = C_count = D_count = 0
 while voter_num < 10
-  voter_num
+  voter_num += 1
   print "Voter ##{voter_num}: "
   input_vote = gets.chomp.upcase
-  until (('A'..'E').includes?(input_vote))  
+  until (('A'..letter).includes?(input_vote))  
     puts "Invalid input. Please enter a valid selection."
     input_vote = gets.chomp.upcase
   end
-  case input_vote
-  when 'A'
-    A_count++
-  when 'B'
-    B_count++
-  when 'C'
-    C_count++
-  when 'D'
-    D_count++
-  when 'E'
-    E_count++
-  end
+  # case input_vote
+  # when 'A'
+  #   A_count++
+  # when 'B'
+  #   B_count++
+  # when 'C'
+  #   C_count++
+  # when 'D'
+  #   D_count++
+  # when 'E'
+  #   E_count++
+  # end
 end
 
 puts "Final tally:/nA: #{A_count}/nB: #{B_count}/nC: #{C_count}/nD: #{D_count}"
